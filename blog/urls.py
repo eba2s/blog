f47c041126bbba889beebfa674a30c24efaaa521
bogdan <eba2s@yahoo.co>
2015-07-09 20:42:15 +0300

    8 iulie fixed all issues

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from blog.views import One_Per_Page
+from blog.views import Blog_Per_Page
 
 
 urlpatterns = patterns('',
@@ -9,6 +10,20 @@ urlpatterns = patterns('',
                        url(r'^latestnews/$', 'blog.views.latestnews', name='latestnews'),
                        url(r'^archive/$', 'blog.views.archive', name='archive'),
 
-                       url(r'^(?P<pk>\d+)$', One_Per_Page.as_view(), name='oneperpage'),
+                       url(r'^(?P<pk>\d+)/', One_Per_Page.as_view(
+                           template_name='blog/aboutme_detail.html'), name="detail"),
+
+                       url(r'^blog/(?P<pk>\d+)/', Blog_Per_Page.as_view(
+                           template_name='blog/blogpost_detail.html'), name="detail"),
+
+                       url(r'^latestnews/(?P<pk>\d+)/', Blog_Per_Page.as_view(
+                           template_name='blog/blogpost_detail.html'), name="detail"),
+
+                       url(r'^archive/(?P<pk>\d+)/', Blog_Per_Page.as_view(
+                           template_name='blog/blogpost_detail.html'), name="detail")
+
+
+
+
 
                        )
